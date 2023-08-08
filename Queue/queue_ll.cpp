b18dcd9b88427eb9ea2c517b38ca91ead4207846
nishant-dongare <nishantdongare@outlook.com>
2023-08-09 01:15:56 +0530

    Queue using linked list:done

---
+++ Queue/queue_ll.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+void traversal();
+void enqueue();
+void dequeue();
+void search();
+void removeAll();
+
+struct node{
+  int value;
+  struct node * next;
+};
+
+struct node * head,* tail;
+
+int main(){
+  for(int i;i!=0;){
+    cout<<"\n1.Traversal\n2.Enqueue\n3.Dequeue\n4.Search\n5.RemoveAll\n";
+    // cout<<"\nEnter your choice : ";
+    cin>>i;
+    switch(i){
+      case 1:traversal();
+        break;
+      case 2:enqueue();
+        break;
+      case 3:dequeue();
+        break;
+      case 4:search();
+        break;
+      case 5:removeAll();
+        break;
+      default:return 0;
+    }
+  }
+}
+
+
+
+void traversal(){
+  if(head==NULL || head==tail) cout<<"Queue is Empty\n";
+  for(struct node * ptr=head ;ptr!=NULL;ptr=ptr->next)
+    cout<<" => "<<ptr->value;
+}
+
+void enqueue(){
+  struct node * newNode =(struct node *) malloc(sizeof(struct node));
+  cout<<"\nEnter your number : ";
+  cin>>newNode->value;
+  if(head == NULL){
+    newNode->next=NULL;
+    head = newNode;
+    tail = newNode;
+  }else{
+    tail->next=newNode;
+    tail=newNode;
+  }
+}
+
+void dequeue(){
+  if(head==NULL || head==tail) cout<<"Queue is Empty\n";
+  else{
+    struct node * ptr;
+    for(ptr=head;ptr->next!=tail;ptr=ptr->next);
+    free(tail);
+    tail=ptr;
+    tail->next=NULL;
+  }
+}
+
+void search(){
+  if(head==NULL || head==tail) cout<<"Queue is Empty\n";
+  else{
+    int value;
+    struct node * ptr;
+    cout<<"\nEnter the Element : ";
+    cin>>value;
+    for(ptr=head;ptr->value=value;ptr=ptr->next);
+  }
+}
+
+void removeAll(){
+  while(head!=NULL){
+    head=tail;
+    head=head->next;
+    free(tail);
+  }
+}
+
