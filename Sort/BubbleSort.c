0efa6e3ea0aada03ba2867843ddea348ed53ff49
nishant-dongare <justanothernormalboy@outlook.com>
2023-09-11 21:40:38 +0530

    update

--- Sort/BubbleSort.c
+++ Sort/BubbleSort.c
@@ -1,25 +1,30 @@
 #include<stdio.h>
 
 void bubbleSort(int[],int);
+void printArray(int arr[],int len);
 void main(){
-  int a[5] = {50,20,10,40,30};
-  int len = sizeof(a)/sizeof(a[0]);
-  bubbleSort(a,len);
+  int arr[5] = {50,20,10,40,30};
+  int len = sizeof(arr)/sizeof(arr[0]);
+  bubbleSort(arr,len);
 }
 
-void bubbleSort(int a[],int len){
+void bubbleSort(int arr[],int len){
   int temp;
-  for(int i=0;i<len-1;i++){
-    for(int j=0;j<len;j++){
-      if(a[j]>a[j+1]){
-        temp=a[j];
-        a[j]=a[j+1];
-        a[j+1]=temp;
+  for(int i=0;i<len;i++){
+    for(int j=0;j<len-1;j++){
+      if(arr[j]>arr[j+1]){
+        temp=arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=temp;
       }
     }
+    printArray(arr,len);
   }
+}
+
+void printArray(int arr[],int len){
   for(int i=0;i<len;i++){
-    printf("=> %d",a[i]);
+    printf("=> %d",arr[i]);
   }
   printf("\n");
 }
