847c6e566d20205eae8e686168036b18e52a37ec
nishant-dongare <justanothernormalboy@outlook.com>
2023-09-14 14:40:47 +0530

    insertion sort done

---
+++ Sort/InsertionSort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+void insertionSort(int arr[],int len){
+  int j,key;
+  for(int i=1;i<len;i++){
+    key=arr[i];
+    j=i-1;
+    while(j>=0 && arr[j]>key){
+        arr[j+1]=arr[j];
+        j--;
+    }
+    arr[j+1]=key;
+  }
+}
+
+void printArray(int arr[],int len){
+  for(int i=0;i<len;i++){
+    printf("=> %d",arr[i]);
+  }
+  printf("\n");
+}
+
+int main(){
+  int arr[5] = {50,20,10,40,30};
+  int len = sizeof(arr)/sizeof(arr[0]);
+  insertionSort(arr,len);
+  printArray(arr,len);
+  return 0;
+}
+
---
+++ Sort/SelectionSort.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std
+
+void swap(int * a,int * b){
+  int temp = b*;
+  b* = a*
+  a* = temp
+}
