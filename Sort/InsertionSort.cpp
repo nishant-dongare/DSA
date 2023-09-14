63c7e4bc304b36b63a407e344c24233ddf22df2e
nishant-dongare <justanothernormalboy@outlook.com>
2023-09-14 14:49:10 +0530

    <=

--- Sort/InsertionSort.cpp
+++ Sort/InsertionSort.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
 
+void printArray(int arr[],int len){
+  for(int i=0;i<len;i++){
+    printf("=> %d",arr[i]);
+  }
+  printf("\n");
+}
+
 void insertionSort(int arr[],int len){
   int j,key;
   for(int i=1;i<len;i++){
@@ -12,20 +19,15 @@ void insertionSort(int arr[],int len){
     }
     arr[j+1]=key;
   }
+  printArray(arr,len);  
 }
 
-void printArray(int arr[],int len){
-  for(int i=0;i<len;i++){
-    printf("=> %d",arr[i]);
-  }
-  printf("\n");
-}
+
 
 int main(){
   int arr[5] = {50,20,10,40,30};
   int len = sizeof(arr)/sizeof(arr[0]);
   insertionSort(arr,len);
-  printArray(arr,len);
   return 0;
 }
 
